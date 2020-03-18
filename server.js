d3401ebb7715e0a2ac511376d0e6833b3ab7bd0c
reddynavya481 <reddynavya481@gmail.com>
2020-03-18 15:46:20 +0530

    second commit

--- server.js
+++ server.js
@@ -12,9 +12,37 @@ mongoose.connect(DB,{
     useNewUrlParser:true,
     useCreateIndex:true,
     useFindAndModify:false
-}).then(con=>{
+}).then(()=>
     console.log("success")
+)
+const tourSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,'specify a value'],
+        unique:true
+    },
+    age:Number,
+    gender:{
+        type:String,
+        required:[true,'gender must have a value']
+    }
+ })
+
+const Tour=mongoose.model('Tour',tourSchema)
+
+const testTour=new Tour({
+    name:'navyareddy',
+    age:21,
+    gender:'female'
 })
+testTour.save().then((doc)=>{
+    console.log(`saved ur contents ${doc}`)
+}).catch(err=>{
+    console.log(err)
+})
+
+
+
 
 
 
